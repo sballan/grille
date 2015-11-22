3123b427d7a1aa6bfef0b58b3522def67f6de9a9
thinkocapo <thinkocapo@gmail.com>
2015-11-21 23:49:30 -0500

    socket communication works between back-end (POST request, Webhooks) and front-end (there's a socket.on listening from the Home Controller)

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,11 +1,17 @@
 'use strict';
 var router = require('express').Router();
 var Webhooks = require('../github-data/webhooks')
+var io = require('../../io')
+
 module.exports = router;
 
 router.use('/members', require('./members'));
 
 router.post('/webhooks', function(req, res, next) {
+	var socket = io()
+	//Can emit this after Webhooks exectues, or inside of Webhooks EventHandler
+	socket.emit('update', {name: "UPDATED sample DATA"})
+	
 	Webhooks[req.headers['x-github-event']](req.body)
 	res.send()
 
--- server/io/index.js
+++ server/io/index.js
@@ -10,6 +10,7 @@ module.exports = function (server) {
 
     io.on('connection', function () {
         // Now have access to socket, wowzers!
+        console.log("IO.ON CONNECTION...")
     });
     
     return io;
