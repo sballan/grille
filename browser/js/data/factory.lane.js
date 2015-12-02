015e75d2aa2f80bf7ee40e9b93454977069b8ab8
Srikant Sarda <ssarda@Srikants-MacBook-Air.local>
2015-12-02 18:41:38 -0500

    added lane factory

--- browser/js/data/factory.lane.js
+++ browser/js/data/factory.lane.js
@@ -1,8 +1,10 @@
 app.factory('LaneFactory', function($http) {
-	return {
+	var LaneFactory =  {
 		createLane: function(newLane) {
 			return $http.post('api/board/lanes/post/' + newLane.boardID + '/' + newLane.title);
 		}
 
 	}
-})
+
+	return LaneFactory;
+});
