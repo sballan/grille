5f90069772c9885bc884e08e6a561002922235f6
thinkocapo <thinkocapo@gmail.com>
2015-12-03 14:38:55 -0500

    changes

--- server/app/routes/board/routes.comments.js
+++ server/app/routes/board/routes.comments.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var Card = require('mongoose').model('Card');
+var User = require('mongoose').model('User');
 var Comment = require('mongoose').model('Comment');
 var Promise = require('bluebird')
 var GitHubApi = require('github')
@@ -18,28 +19,37 @@ router.post('/post/:cardID', function(req,res,next){
 		token: req.user.accessToken
 	});
 
-	//var createCommentAsync = Promise.promisify(github.issues.createComment);
+	var createCommentAsync = Promise.promisify(github.issues.createComment);
+	console.log("~~~ROUTER, req.body.card", req.body.card)
+	/*
+	var msg = {
+		user: req.user, //need user name str
+		repo: req.body.card.githubID, //repo name str
+		number: req.params.cardID,
+		body: req.body.comment.body
+	}
 
-	// createCommentAsync({ 
-	// 	user: req.user
-	// 	repo: req.body.card.githubID
-	// 	number: req.params.cardID,
-	// 	body: req.body.body
-	// })
+	User.findOne({ githubID: req.user})
+	.then(function(user){
+		msg.user = user.username
+		// return Board.findOne({ githubID: req.body.card.board })
+	
+	})
 
+	createCommentAsync(msg)
+	.then(function(response){
+		//parse it
+		//create new Comment({})
+	})
+	*/
 	//2 persist comment to our DB
 	console.log("~~~ROUTER , reqbody:", req.body)
-
 	Card.findOne({ githubID: req.params.cardID})
 	.then(function(card){
-		console.log("~~~ROUTER , reqbody postCardFindOne is:", req.body)
-
 		var newComment = new Comment({ body: req.body.comment.body, author: req.user })
-
 		card.comments.push(newComment)
 		card.save()
 		console.log("~~~ROUTER newly updated card:", card)
 		res.send(card)
 	})
-	// .then(function())
 })
