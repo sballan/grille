764cf6c05821f8a9cd3d592c6d96cddb34536aee
[David Scherban] <[davidscherban@gmail.com]>
2015-12-03 10:35:23 -0500

    dont do this at home kids

---
+++ server/app/routes/cards/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var router = express.Router();
+var GitHubApi = require('github')
+
+
+router.post("/",function(req,res,next){
+	var github = req.user.githubAccess;
+})
+router.put("/",function(req,res,next){
+	var github = req.user.githubAccess;
+})
+
+router.delete("/",function(req,res,next){
+	var github = req.user.githubAccess;
+})
+
+module.exports = router;
