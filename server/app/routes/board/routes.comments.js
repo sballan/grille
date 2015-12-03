46b5dc633e6312f953e9a124ec0c196b04680e27
thinkocapo <thinkocapo@gmail.com>
2015-12-03 16:04:45 -0500

    can add commits. it updates front-end, and saves to github. refresh and it saves to db

--- server/app/routes/board/routes.comments.js
+++ server/app/routes/board/routes.comments.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var Card = require('mongoose').model('Card');
 var User = require('mongoose').model('User');
+var Board = require('mongoose').model('Board');
 var Comment = require('mongoose').model('Comment');
 var Promise = require('bluebird')
 var GitHubApi = require('github')
@@ -8,7 +9,6 @@ var GitHubApi = require('github')
 module.exports = router;
 
 router.post('/post/:cardID', function(req,res,next){
-	//1 tell Github that we added a comment
 	var github = new GitHubApi({
 		debug: true,
 		version: "3.0.0"
@@ -19,37 +19,39 @@ router.post('/post/:cardID', function(req,res,next){
 		token: req.user.accessToken
 	});
 
-	var createCommentAsync = Promise.promisify(github.issues.createComment);
-	console.log("~~~ROUTER, req.body.card", req.body.card)
-	/*
+	
 	var msg = {
-		user: req.user, //need user name str
-		repo: req.body.card.githubID, //repo name str
-		number: req.params.cardID,
+		user: null,
+		repo: null,
+		number: req.body.card.issueNumber,
 		body: req.body.comment.body
 	}
 
-	User.findOne({ githubID: req.user})
-	.then(function(user){
-		msg.user = user.username
-		// return Board.findOne({ githubID: req.body.card.board })
-	
+	Board.findById(req.body.card.board)
+	.then(function(board){
+		msg.repo = board.name
+		msg.user = board.owner.username
+		var createCommentAsync = Promise.promisify(github.issues.createComment);
+		console.log("msg:", msg)
+		return createCommentAsync(msg)
 	})
-
-	createCommentAsync(msg)
 	.then(function(response){
-		//parse it
-		//create new Comment({})
+		//parse the response...
+		console.log("responded~~~~~~~~~")
+		res.send(response)
+		//create new comment...
 	})
-	*/
+	// .catch(console.log)
+
+	//console.log
 	//2 persist comment to our DB
-	console.log("~~~ROUTER , reqbody:", req.body)
-	Card.findOne({ githubID: req.params.cardID})
-	.then(function(card){
-		var newComment = new Comment({ body: req.body.comment.body, author: req.user })
-		card.comments.push(newComment)
-		card.save()
-		console.log("~~~ROUTER newly updated card:", card)
-		res.send(card)
-	})
+	// console.log("~~~ROUTER , reqbody:", req.body)
+	// Card.findOne({ githubID: req.params.cardID})
+	// .then(function(card){
+	// 	var newComment = new Comment({ body: req.body.comment.body, author: req.user })
+	// 	card.comments.push(newComment)
+	// 	card.save()
+	// 	console.log("~~~ROUTER newly updated card:", card)
+	// 	res.send(card)
+	// })
 })
