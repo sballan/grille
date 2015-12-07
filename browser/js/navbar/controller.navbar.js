a0ec5cdbb9931c06b08cd1241f117c4ebd4fc89a
Srikant Sarda <ssarda@Srikants-MacBook-Air.local>
2015-12-07 18:32:03 -0500

    fixed repo search and updated boardname location

--- browser/js/navbar/controller.navbar.js
+++ browser/js/navbar/controller.navbar.js
@@ -53,5 +53,9 @@ app.controller('NavBarCtrl', function($scope, $rootScope, $uibModal, AuthService
 
   $scope.currentBoard = BoardFactory.getCurrentBoard;
 
-  $scope.getRepoCache = GitHubFactory.getRepoCache;
+  $scope.getActiveRepoCache = function() {
+    return GitHubFactory.getCache().filter(function(repo) {
+      return (repo.isActive);
+    })
+  }
 })
