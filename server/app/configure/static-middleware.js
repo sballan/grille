521348f8f6ba733f53741c42842f2f0f13cdad82
Samuel Ballan <sgb4622@gmail.com>
2015-12-08 18:07:37 -0500

    initialized:

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -10,12 +10,13 @@ module.exports = function (app) {
     var npmPath = path.join(root, './node_modules');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
-    var bowerPath = path.join(root,'./bower_components')
+    var mediaPath = path.join(root, './server/app/media');
+
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
-    app.use(express.static(bowerPath));
+    app.use(express.static(mediaPath));
 
 };
