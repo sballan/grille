c5b88cac3c3df1e15dfec9534803c6a1a963b9e5
Samuel Ballan <sgb4622@gmail.com>
2015-12-08 17:30:06 -0500

    fixed deployment variables

--- server/env/development.js
+++ server/env/development.js
@@ -1,21 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/grille-tester",
   "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
-  },
-  "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
-  },
-  "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
-  },
   "GITHUB":{
     "clientID":"2528aa9677905b6b4ce6",
     "clientSecret": "b4e25203817b82125ed7e6426c7ebc6ef1125a41",
