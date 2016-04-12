1b81f9809325ccc4bb32ca79a0d048ad32e2d267
Samuel Ballan <sgb4622@gmail.com>
2016-04-12 15:54:19 -0400

    middle of refactoring routes again

--- server/app/routes/issues/routes.issues.js
+++ server/app/routes/issues/routes.issues.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const Promise = require('bluebird');
 const Griller = require('../../../modules/griller');
 
-module.exports = router
+module.exports = router;
 
 router.param('issueId', function(req, res, next, id) {
   req.griller = new Griller(req, res, next);
-  return req.griller.attach('Issue', id)
-    .then(function() {
-      console.log(`Issue #${req.theIssue.issueNumber} was attached.`)
+  return req.griller.attach('Repo', {issues: {$elemMatch: {_id: id}}}, 'owner')
+    .then(function(g) {
+      console.log(`Repo ${g.repo.name} was attached; about to get Issue.`);
       next()
     })
 });
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:issueId', function(req, res, next) {
-  req.griller.getOneIssue()
+  req.griller.getOneIssue(null, req.params.issueId)
       .then(function(issue) {
         if(issue) res.json(issue);
         else res.sendStatus(404);
