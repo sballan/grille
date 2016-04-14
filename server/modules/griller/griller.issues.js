e86cffe13eb6bf4c918ce5906c73a4a699c4143f
Samuel Ballan <sgb4622@gmail.com>
2016-04-14 19:00:57 -0400

    some polish on giller.issues

--- server/modules/griller/griller.issues.js
+++ server/modules/griller/griller.issues.js
@@ -2,55 +2,55 @@
 var Promise = require('bluebird');
 
 // Returns req
-const getAll = function(g, repo=this.repo) {
-  const self = !!g ? g : this;
-  self.repo = repo;
-  // repo = self.repo = repo || self.repo;
+const getAll = function(G=this, repo=G.repo) {
+  if(!repo) return Promise.reject('No repo argument or on the Griller object');
+  G.repo = repo;
 
   let config = {
     sort: 'updated',
     state: "all"
   };
 
-  return self.Core.githubGet(self, config, self.client.issues.repoIssues)
-    .then(function(rawIssues) {
-      return self.Parse.issues(rawIssues, self.repo)
+  return G.Core.githubGet(G, config, G.client.issues.repoIssues)
+    .then(rawIssues=> {
+      return G.Parse.issues(rawIssues, G.repo)
     })
-    .then(function(allIssues) {
-      g.repo.issues = allIssues;
-      g.issues = allIssues;
-      console.log(`${g.issues.length} Issues attached.`);
-      return self.Core.dbSave(g.repo, 'issues')
+    .then(allIssues=> {
+      G.repo.issues = allIssues;
+      G.issues = allIssues;
+
+      console.log(`${G.issues.length} Issues attached.`);
+      return G.Core.dbSave(G.repo, 'issues')
     })
-    .then(()=>{
-      return self
-    })
+    .then(()=>G)
 
 };
 
-const getOne = function(repo, issue) {
-  const self = !!g ? g : this;
-  self.repo = repo || self.repo;
-  self.issue = issue || self.issue;
+const getOne = function(G=this, repo=G.repo, issue=G.issue) {
+  if(!repo || !issue) return Promise('No repo or issue as argument or on Griller object');
+
+  // TODO consider making new Griller object here
+  G.repo = repo;
+  G.issue = issue;
 
   let config= {
     number: issue.issueNumber,
     state: "all"
   };
 
-  return self.Core.githubGet(self, config, self.client.issues.repoIssue)
-    .then(self.Parse.issues)
-    .then(function(issue) {
-      if(issue.hasComments) {
-        return self.Comments.getAllForIssue(self)
+  return G.Core.githubGet(G, config, G.client.issues.repoIssue)
+    .then(G.Parse.issues)
+    .then(dbIssue=> {
+      if(dbIssue.hasComments) {
+        return G.Comments.getAllForIssue(G)
       }
-      return issue
+      return dbIssue
     })
-    .then(function(issue) {
-      self.repo.issues.push(issue);
-      return self.Core.dbSave(repo, 'issues')
+    .then(dbIssue=> {
+      G.repo.issues.push(dbIssue);
+      return G.Core.dbSave(G.repo, 'issues')
     })
-    .then(()=>self)
+    .then(()=>G)
 };
 
 module.exports = (context=this)=> ({
